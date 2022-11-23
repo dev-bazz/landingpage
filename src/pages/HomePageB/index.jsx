37b30c40a7e811098809556e821902e3932c19bf
bazz <clement.f.bazz@gmail.com>
2022-11-23 15:55:36 +0100

    Let Me See u brake now

--- src/pages/HomePageB/index.jsx
+++ src/pages/HomePageB/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./pageB.module.scss";
 import imag3d from "./assets/Other 14.webp";
 import upload from "./assets/Group 1000000932.png";
-import NavBar from "../../components/navBar/index";
-import Footer from "../../components/footer/index";
-import CustomerReview from "../HomePageA/CustomerReview/index";
-import BrandsLogo from "../HomePageA/BrandLogos/index";
-import Hero from "../HomePageA/heroSection/index";
+import NavBar from "../../components/navBar/index.jsx";
+import Footer from "../../components/footer/index.jsx";
+import CustomerReview from "../HomePageA/CustomerReview/index.jsx";
+import BrandsLogo from "../HomePageA/BrandLogos/index.jsx";
+import Hero from "../HomePageA/heroSection/index.jsx";
 
 function HomePageB() {
   return (
